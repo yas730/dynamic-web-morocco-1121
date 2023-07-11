afeef8f5aefa31d4a3141b03b499c951d3b4dbf3
yas730 <113551442+yas730@users.noreply.github.com>
2023-07-11 13:21:59 +0300

    Update ytdown2.js

--- plugins/ytdown2.js
+++ plugins/ytdown2.js
@@ -2,7 +2,7 @@ import fg from 'api-dylux'
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 let limit = 350
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-	if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
+	if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTlFFwxtXI`
     if (!args[0].match(/youtu/gi)) throw `❎ Verify that the YouTube link`
 	 let chat = global.db.data.chats[m.chat]
 	try {
